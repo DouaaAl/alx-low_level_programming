2bb68248512b984039d69c0579c645585c208817
DouaaAl <elmahraouidouaa123@gmail.com>
2023-09-18 15:43:25 -0700

    5.1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints text
+ *
+ * Return: returns 0 when success
+ */
+
+int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,");
+}
