c49be16838117b0e6714edd6a4bfa4bf83559b4d
DouaaAl <elmahraouidouaa123@gmail.com>
2023-09-19 15:41:35 -0700

    101.1

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i, j, m;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		for (j = 49 ; j < 58 ; j++)
+		{
+			for (m = 50 ; m < 58 ; m++)
+			{
+				if (m > j && j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(m);
+					if (i != 55 || j != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}	
+	}
+	putchar('\n');
+	return (0);
+}
