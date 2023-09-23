84fe4e1e6fa0c5a695e5b0529483dd25921a4ef0
DouaaAl <elmahraouidouaa123@gmail.com>
2023-09-23 07:08:52 -0700

    8.1

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0 ; i < 24 ; i++)
+	{
+		for (j = 0 ; j < 60 ; j++)
+		{
+			if ( i < 10)
+			{
+				_putchar('0');
+				_putchar(i + '0');
+			}
+
+			else
+				_putchar(i + '0');
+
+			_putchar(':');
+
+			if (j < 10)
+			{
+				_putchar('0');
+				_putchar(j + '0');
+			}
+
+			else
+				_putchar(j + '0');
+		}
+	}
+}
