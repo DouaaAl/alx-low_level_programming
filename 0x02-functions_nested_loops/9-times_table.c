7595a2341889a9aa1c2f8a268d9adc879e639e60
DouaaAl <elmahraouidouaa123@gmail.com>
2023-09-23 07:57:46 -0700

    9.6

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,8 @@ void times_table(void)
 			if (j != 0)
 				_putchar(' ');
 
-			_putchar((i*j)/10 + '0');
+			if((i*j) >= 10)
+				_putchar((i*j)/10 + '0');
 			_putchar((i*j)%10 + '0');
 
 			if (j != 9)
