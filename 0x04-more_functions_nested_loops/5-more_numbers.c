8d81306234a9443b7d014939d4539b3e942c2ac3
DouaaAl <elmahraouidouaa123@gmail.com>
2023-09-24 03:33:40 -0700

    5.1

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints numbers
+ * Return: returnes nothing
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = '0' ; j < '15' ; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
