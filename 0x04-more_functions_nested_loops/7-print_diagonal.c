94acb7c1d943763467a2ac72e9e73e87233c61a2
DouaaAl <elmahraouidouaa123@gmail.com>
2023-09-24 04:13:21 -0700

    7.1

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that prints diagnlos
+ * @n: function param
+ * Return: returns nothing
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		for (j = 0; j <= i ; j++)
+		{
+			if (j == i){
+				_putchar('\');
+			}
+			else{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+
+	if (n < 0)
+		_putchar('\n');
+}
