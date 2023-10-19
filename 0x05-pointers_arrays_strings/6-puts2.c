8da1e06600888d93b975e7378196db0fc4cc9ec8
DouaaAl <elmahraouidouaa123@gmail.com>
2023-10-18 22:14:04 -0700

    6.1

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - our main function
+ * @str: function param
+ * Return: returns nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i = i+2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
