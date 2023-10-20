66d9919df8648fbf9f681c21fda9a9944b52736b
DouaaAl <elmahraouidouaa123@gmail.com>
2023-10-20 00:43:02 -0700

    1.1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strncat - Our Function
+ * @dest: first function Param
+ * @src: second function param
+ * @n: third function param
+ * Return: returns nothing
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = strlen(dest);
+	int i;
+
+	for (i = 0 ; i < n ; src != '\0' ; i++)
+	{
+		dest[len + i] = *src[i];
+		src++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,5 +3,7 @@
 
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 
 #endif
