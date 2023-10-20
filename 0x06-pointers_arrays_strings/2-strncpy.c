49358cbe590bd5bb43b6200655b3925a67c93e0e
DouaaAl <elmahraouidouaa123@gmail.com>
2023-10-20 00:58:59 -0700

    2.1

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strncpy - Main function
+ * @dest: first function param
+ * @src: second function param
+ * @n: third function param
+ * Return: returns char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[i] = src[i]
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
