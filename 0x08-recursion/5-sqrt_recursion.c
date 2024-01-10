5562c1a0e1a4321a17d0205d9fe66f65bfada50d
DouaaAl <elmahraouidouaa123@gmail.com>
2024-01-10 11:49:14 -0800

    5.4

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "find_sqrt.c"
+
+/**
+ * _sqrt_recursion - sqrt function
+ * @n: param
+ * Return: returns
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (find_sqrt(n, n));
+}
---
+++ 0x08-recursion/find_sqrt.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * find_sqrt - function that prints sqrt
+ * @num: fisr param
+ * @curr: second param
+ * Return: return the sqrt
+ */
+
+int find_sqrt(int num, int curr)
+{
+        if (curr < 0)
+                return (-1);
+        else if ((curr * curr) == num)
+                return (curr);
+        return (find_sqrt(num, curr - 1));
+}
