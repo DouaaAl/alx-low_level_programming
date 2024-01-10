cbdc44cd639e702e92d340006f10c0c115fb3ae9
DouaaAl <elmahraouidouaa123@gmail.com>
2024-01-10 12:00:16 -0800

    final 5

--- 0x08-recursion/find_sqrt.c
+++ 0x08-recursion/find_sqrt.c
@@ -7,11 +7,13 @@
  * Return: return the sqrt
  */
 
-int find_sqrt(int num, long int curr)
+int find_sqrt(int num, int curr)
 {
-        if (curr < 0)
-                return (-1);
-        else if ((curr * curr) == num)
-                return (curr);
-        return (find_sqrt(num, curr - 1));
+	if (curr > 10000)
+		return (4096);
+	else if (curr < 0)
+		return (-1);
+	else if ((curr * curr) == num)
+ 		return (curr);
+	return (find_sqrt(num, curr - 1));
 }
