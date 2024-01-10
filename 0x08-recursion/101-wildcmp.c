58eaa1f0a87edc3d561216b1a91eccc4da9b75e1
DouaaAl <elmahraouidouaa123@gmail.com>
2024-01-10 15:44:13 -0800

    101-2

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,11 +9,12 @@
 
 int wildcmp(char *s1, char *s2)
 {
-        char *nexts2;
-        nexts2 = s2 + 1;
+	char *nexts2;
 
-        if(*s2 == '*')
-        {
+	nexts2 = s2 + 1;
+
+	if (*s2 == '*')
+	{
 		if ((*nexts2 != '*') && (*nexts2 != '\0'))
 		{
 			if ((*s1 != *nexts2) && (*s1 != '\0'))
@@ -22,14 +23,17 @@ int wildcmp(char *s1, char *s2)
 				return (0);
 			s2 = nexts2;
 		}
+
 		else if (*nexts2 == '\0')
 		{
 			return (1);
 		}
-		else {
+
+		else
+		{
 			return (wildcmp(s1, s2 + 1));
 		}
-        }
+	}
 
 	if (*s2 != *s1)
 		return (0);
