5a84cc6d0e17018738e1af1e16c1413f3fb8e06a
DouaaAl <elmahraouidouaa123@gmail.com>
2024-01-12 15:15:17 -0800

    A.0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: first argument
+ * @argv: second argument
+ * Return: returns integer
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
