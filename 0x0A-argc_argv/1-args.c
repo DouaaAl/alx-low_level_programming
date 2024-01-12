1077518acd108c602823e09b0f6c8e735422507e
DouaaAl <elmahraouidouaa123@gmail.com>
2024-01-12 15:19:06 -0800

    A.1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main funtcion
+ * @argc: first argument
+ * @argv: second argument
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
