9065385692320e394a901b5dced39e722c9072fc
DouaaAl <elmahraouidouaa123@gmail.com>
2024-01-12 15:55:24 -0800

    A.4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - main function done
+ * @argc: first argument
+ * @argv: second argument
+ * Return: retuens
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, count;
+
+	if (argc == 1)
+		printf("0\n");
+	for ( i = 1 ; i < argc ; i++)
+	{
+		for ( j = 0 ; argv[i][j] != '\0' ; j++)
+		{
+			if(!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		count += atoi(argv[i]);
+	}
+
+	printf("%d\n", count);
+	return (0);
+}
