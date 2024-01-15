e757f91b70b59ea523f863623f95c0da69ebe94a
DouaaAl <elmahraouidouaa123@gmail.com>
2024-01-15 12:08:56 -0800

    B2.0

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - string function
+ * @s1: first param
+ * @s2: second param
+ * Return: returns NULL or a pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, count1 = 0;
+	int count2 = 0;
+	char *strcn;
+
+	for (i = 0 ; s1[i] != '\0' ; i++)
+		count1 += 1;
+	for (i = 0 ; s2[i] != '\0' ; i++)
+		count2 += 1;
+	strcn = (char *)malloc(sizeof(char) * (count1 + count2 + 1));
+
+	for (i = 0 ; s1[i] != '\0' ; i++)
+		strcn[i] = s1[i];
+	for (i = 0 ; s2[i] != '\0' ; i++)
+		strcn[count1 + i] = s2[i];
+
+	strcn[(count1 + count2)] = '\0';
+	if (strcn == NULL)
+		return (NULL);
+	return (strcn);
+}
