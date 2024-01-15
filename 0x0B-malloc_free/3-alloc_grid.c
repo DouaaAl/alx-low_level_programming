eea75299cf04e6d4e76b26eb2065c0310524009a
DouaaAl <elmahraouidouaa123@gmail.com>
2024-01-15 15:16:58 -0800

    B.3.0

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function
+ * @width: width of array
+ * @height: height of array
+ * Return: returns array or NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **arr;
+
+	if ((width <= 0) || (height <= 0))
+		return (NULL);
+	arr = (int **)malloc(sizeof(int*) * width);
+
+	for (i = 0 ; i < width ; i++)
+		arr[i] = (int *)malloc(height * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0 ; i < width ; i++)
+	{
+		for (j = 0 ; j < height ; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
+}
