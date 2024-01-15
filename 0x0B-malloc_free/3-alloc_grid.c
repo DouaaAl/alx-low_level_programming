276395dbc0be06a21d5e6d5bddd432ddbde45d14
DouaaAl <elmahraouidouaa123@gmail.com>
2024-01-15 15:25:30 -0800

    B.3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,6 +18,9 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0 ; i < width ; i++)
 		arr[i] = (int *)malloc(height * sizeof(int));
+
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0 ; i < width ; i++)
 	{
 		if (arr[i] == NULL)
