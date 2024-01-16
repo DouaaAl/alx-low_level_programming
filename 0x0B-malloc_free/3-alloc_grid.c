a1f7cff6a999f28c9895a70562bfd2ce37097529
DouaaAl <elmahraouidouaa123@gmail.com>
2024-01-16 02:43:11 -0800

    B.3.3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,21 +14,23 @@ int **alloc_grid(int width, int height)
 
 	if ((width <= 0) || (height <= 0))
 		return (NULL);
-	arr = (int **)malloc(sizeof(int *) * width);
-
-	for (i = 0 ; i < width ; i++)
-		arr[i] = (int *)malloc(height * sizeof(int));
+	arr = malloc(sizeof(int **) * height);
 
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0 ; i < width ; i++)
+
+	for (i = 0 ; i < height ; i++)
 	{
+		arr[i] = malloc(sizeof(int *) * width);
+
 		if (arr[i] == NULL)
-			return (NULL);
-	}
-	for (i = 0 ; i < width ; i++)
-	{
-		for (j = 0 ; j < height ; j++)
+		{
+			free(arr);
+			for (j = 0 ; j < height ; j++)
+				free(arr[i]);
+		}
+
+		for (j = 0 ; j < width ; j++)
 			arr[i][j] = 0;
 	}
 	return (arr);
