68f274dbbd4c8c4b10c5826b92311f563fd8c100
DouaaAl <elmahraouidouaa123@gmail.com>
2024-01-18 10:33:35 -0800

    C.0.0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - main function
+ * @b: function param
+ * Return: returnes nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void* *pr = (void *)malloc(b);
+
+	if (pr == NULL)
+	{
+		exit(0);
+	}
+
+	return (pr);
+}
