96124cbe050ff22709c649546d5afda7b998d9c2
DouaaAl <elmahraouidouaa123@gmail.com>
2024-01-25 08:19:57 -0800

    10.0.0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - main function
+ * @n: n param
+ * Return: returns int
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int s = 0, i;
+
+	if (n == 0)
+		return (0);
+	va_start(args, n);
+
+	for (i = 0 ; i < n ; i++)
+	{
+		int x = va_arg(args, int);
+		s+= x;
+	}
+
+	va_end(args);
+	return (s);
+}
