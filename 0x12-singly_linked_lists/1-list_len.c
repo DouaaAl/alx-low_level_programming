91a0996f1c23662a15c15ff6b25a8760c2c99493
DouaaAl <elmahraouidouaa123@gmail.com>
2024-02-01 15:41:25 -0800

    12.1.0

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - our functions
+ * @h: parameter 
+ * Return: returns a list of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
