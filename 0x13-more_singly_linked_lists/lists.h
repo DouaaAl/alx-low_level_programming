78f8fec3593c0a65d5dab73f2e3c512bbd8bb5a4
DouaaAl <elmahraouidouaa123@gmail.com>
2024-02-06 09:50:35 -0800

    13.1.0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -15,5 +16,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif
