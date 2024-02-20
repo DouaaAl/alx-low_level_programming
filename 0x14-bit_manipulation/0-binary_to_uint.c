ad352323d672b59eeaad7f2ed733ba30cfe3699d
DouaaAl <elmahraouidouaa123@gmail.com>
2024-02-20 03:32:47 -0800

    14.0.3

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,20 +13,20 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0, mul = 1, i;
 
-	if (!*b)
+	if (!b)
 		return (0);
-	for (i = 1 ; b[i] != '\0' ; i++)
+	for (i = 0 ; b[i] != '\0' ; i++)
 		mul *= 2;
 
 	for (i = 0 ; b[i] != '\0' ; i++)
 	{
+		mul /= 2;
 		if (b[i] != '1' && b[i] != '0')
 		{
 			return (0);
 		}
 		if (b[i] == '1')
 			num = num + mul;
-		mul /= 2;
 	}
 	return (num);
 }
